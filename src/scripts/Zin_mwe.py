de1f9e2cf96121568f38c16373b04f07018dfae4
Liam Dubay <liam.dubay@gmail.com>
2024-07-17 13:03:19 -0400

    infall metallicities

--- src/scripts/Zin_mwe.py
+++ src/scripts/Zin_mwe.py
@@ -1,15 +1,16 @@
 import vice
 import numpy as np
 
+output_name = '../data/onezone/test_Zin'
 sz = vice.singlezone(
-        name='test_Zin',
-        func=lambda t: 1,
+        name=output_name,
+        func=lambda t: 3,
         mode='ifr',
         elements=('fe', 'o'),
-        Zin=1e-4,
-        Mg0=0,
+        Zin={'fe': 1e-4},
+        Mg0=1,
         dt=0.01,
     )
 sz.run(np.arange(0, 10, 0.01), overwrite=True)
-hist = vice.history('test_Zin')
+hist = vice.history(output_name)
 print(hist)
