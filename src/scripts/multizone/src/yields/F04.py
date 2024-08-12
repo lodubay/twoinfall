67b468a077cfa9228e58578c51e423e30f61e4c9
Liam Dubay <liam.dubay@gmail.com>
2024-08-12 12:08:55 -0400

    update F04 yields

--- src/scripts/multizone/src/yields/F04.py
+++ src/scripts/multizone/src/yields/F04.py
@@ -4,6 +4,7 @@ Francois et al. (2004, A&A 421, 613)
 """
 
 import warnings
+from pathlib import Path
 import numpy as np
 import vice
 
@@ -13,10 +14,10 @@ warnings.filterwarnings('ignore', category=vice.ScienceWarning)
 # CC SNe
 # O yield is unmodified from Woosley & Weaver (1995)
 from vice.yields.ccsne import WW95
-WW95.set_params(m_upper=100)
+# WW95.set_params(m_upper=100)
 
 # Read table of yields by progenitor mass
-data = np.loadtxt('francois2004_table1.dat')
+data = np.loadtxt(Path(__file__).parents[0] / 'francois2004_table1.dat')
 masses = data[:,0]
 imf = np.array([vice.imf.kroupa(m) for m in masses])
 # Trapezoidal rule
@@ -27,7 +28,7 @@ integral /= np.sum([m * vice.imf.kroupa(m) * 0.1 for m in np.arange(0.1, 100.1, 
 # Assign yields
 vice.yields.ccsne.settings['mg'] = integral[0]
 vice.yields.ccsne.settings['si'] = integral[1]
-vice.yields.ccsne.settings['fe'] = integral[2]
+# vice.yields.ccsne.settings['fe'] = integral[2]
 
 # SNe Ia
 from vice.yields.sneia import iwamoto99
