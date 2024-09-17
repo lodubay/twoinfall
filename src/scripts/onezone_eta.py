move equilibrium_mass_loading, fix ampratio calculation

src/scripts/onezone_eta.py
@@ -9,8 +9,7 @@ import matplotlib.pyplot as plt
 import vice
 
 from multizone.src.yields import W23
-from multizone.src.disks import equilibrium_mass_loading
-from multizone.src.models import twoinfall, twoinfall_sf_law
+from multizone.src import models
 from track_and_mdf import setup_figure, plot_vice_onezone
 import paths
 from _globals import ONEZONE_DEFAULTS, ZONE_WIDTH
@@ -30,13 +29,15 @@ def main():
     
     simtime = np.arange(0, 13.21, 0.01)
     
-    eta_func = equilibrium_mass_loading(tau_star=2., tau_sfh=10., alpha_h_eq=0.1)
+    eta_func = models.equilibrium_mass_loading(
+        tau_star=2., tau_sfh=10., alpha_h_eq=0.1
+    )
     
     area = np.pi * ((RADIUS + ZONE_WIDTH)**2 - RADIUS**2)
     
     sz = vice.singlezone(
         name = name,
-        func = twoinfall(
+        func = models.twoinfall(
             RADIUS, 
             first_timescale=1., 
             second_timescale=10., 
@@ -46,7 +47,7 @@ def main():
     )
     sz.eta = eta_func(RADIUS)
     print(eta_func(RADIUS))
-    sz.tau_star = twoinfall_sf_law(area, onset=ONSET)
+    sz.tau_star = models.twoinfall_sf_law(area, onset=ONSET)
     sz.run(simtime, overwrite=True)
     
     plot_vice_onezone(name, fig=fig, axs=axs)

src/scripts/onezone_params.py
@@ -13,10 +13,9 @@ import paths
 # from multizone.src.yields import J21
 # from vice.yields.presets import JW20
 from multizone.src.yields import W23
-vice.yields.sneia.settings['fe'] *= (1.1/1.2)
+# vice.yields.sneia.settings['fe'] *= (1.1/1.2)
 # from multizone.src.yields import F04
-from multizone.src import models, dtds
-from multizone.src.disks import equilibrium_mass_loading
+from multizone.src import models
 from _globals import END_TIME, ONEZONE_DEFAULTS, TWO_COLUMN_WIDTH, ZONE_WIDTH
 from colormaps import paultol
 from track_and_mdf import setup_axes, plot_vice_onezone
@@ -33,8 +32,8 @@ LABELS = {
     'second_timescale': '\\tau_2',
     'onset': 't_{\\rm on}'
 }
-XLIM = (-1.7, 0.7)
-YLIM = (-0.16, 0.54)
+XLIM = (-1.7, 0.8)
+YLIM = (-0.18, 0.54)
 
 def main():
     plt.style.use(paths.styles / 'paper.mplstyle')
@@ -42,32 +41,27 @@ def main():
     fig = plt.figure(figsize=(TWO_COLUMN_WIDTH, 0.36*TWO_COLUMN_WIDTH))
     gs = fig.add_gridspec(7, 22, wspace=0.)
     subfigs = [fig.add_subfigure(gs[:,i:i+w]) for i, w in zip((0, 8, 15), (8, 7, 7))]
-    # Outflow mass-loading factor
-    eta_func = equilibrium_mass_loading(alpha_h_eq=0.1, tau_sfh=10., tau_star=2.)
-    eta = eta_func(RADIUS)
-    # eta = 1.
-    print('Mass-loading factor: ', eta)
-    print('First timescale')
+    print('\nFirst timescale')
     axs0 = vary_param(subfigs[0], first_timescale=[0.1, 0.3, 1, 3],
-                      second_timescale=10, onset=4, eta=eta,
-                      xlim=XLIM, ylim=YLIM, label_index=3)
-    print('Second timescale')
-    axs1 = vary_param(subfigs[1], second_timescale=[1, 3, 10, 30],
-                      first_timescale=1, onset=4, eta=eta,
+                      second_timescale=10, onset=4,
+                      xlim=XLIM, ylim=YLIM, label_index=0, verbose=True)
+    print('\nSecond timescale')
+    axs1 = vary_param(subfigs[1], second_timescale=[3, 5, 10, 30],
+                      first_timescale=1, onset=4,
                       xlim=XLIM, ylim=YLIM, ylabel=False,
-                      label_index=1)
-    print('Onset time')
+                      label_index=1, verbose=True)
+    print('\nOnset time')
     axs2 = vary_param(subfigs[2], onset=[1, 2, 3, 4, 5],
-                      first_timescale=1, second_timescale=10, eta=eta,
+                      first_timescale=1, second_timescale=10,
                       xlim=XLIM, ylim=YLIM, ylabel=False,
-                      label_index=2)
+                      label_index=2, verbose=True)
     plt.subplots_adjust(bottom=0.13, top=0.98, left=0.16, right=0.98, wspace=0.5)
     fig.savefig(paths.figures / 'onezone_params', dpi=300)
     plt.close()
 
 
 def vary_param(subfig, first_timescale=0.1, second_timescale=3, onset=3,
-               label_index=0, eta=0.6, **kwargs):
+               label_index=0, verbose=False, **kwargs):
     """
     Plot a series of onezone model outputs, varying one parameter of the 
     two-infall model while holding the others fixed.
@@ -112,11 +106,19 @@ def vary_param(subfig, first_timescale=0.1, second_timescale=3, onset=3,
     simtime = np.arange(0, END_TIME + dt, dt)
     area = np.pi * ((RADIUS + ZONE_WIDTH)**2 - RADIUS**2)
 
-    for i, val in tqdm(enumerate(values)):
+    for i, val in enumerate(values):
         param_dict[var] = val
+        # Outflow mass-loading factor
+        eta_func = models.equilibrium_mass_loading(
+            alpha_h_eq=0.2, 
+            tau_sfh=param_dict['second_timescale'], 
+            tau_star=2.
+        )
+        eta = eta_func(RADIUS)
         # Run one-zone model
         name = output_name(*param_dict.values())
-        ifr = models.twoinfall(RADIUS, dt=dt, **param_dict)
+        ifr = models.twoinfall(RADIUS, dt=dt, outflows='equilibrium', 
+                               **param_dict)
         sz = vice.singlezone(name=name,
                              func=ifr, 
                              mode='ifr',
@@ -131,6 +133,15 @@ def vary_param(subfig, first_timescale=0.1, second_timescale=3, onset=3,
                           label=f'{val:.1f}', 
                           marker_labels=(i==label_index),
                           markers=[0.3, 1, 3, 10])
+        if verbose:
+            print('Value:', val)
+            print('Eta:', eta)
+            # Thick-to-thin ratio
+            hist = vice.history(name)
+            onset_idx = int(ifr.onset / dt)
+            thick_disk_mass = hist['mstar'][onset_idx-1]
+            thin_disk_mass = hist['mstar'][-1] - thick_disk_mass
+            print('Thick-to-thin ratio:', thick_disk_mass / thin_disk_mass)
 
     # Adjust axis limits
     axs[1].set_ylim(bottom=0)

src/scripts/onezone_sfr.py
@@ -9,8 +9,7 @@ import matplotlib.pyplot as plt
 import vice
 
 from multizone.src.yields import W23
-from multizone.src.disks import equilibrium_mass_loading
-from multizone.src.models import twoinfall, twoinfall_sf_law
+from multizone.src.models import twoinfall, twoinfall_sf_law, equilibrium_mass_loading
 from track_and_mdf import setup_figure, plot_vice_onezone
 from apogee_sample import APOGEESample
 import paths
@@ -19,7 +18,9 @@ from _globals import ONEZONE_DEFAULTS, ZONE_WIDTH
 from colormaps import paultol
 
 RADIUS = 8.
-ONSET = 3.5 # Gyr
+ONSET = 3.5
+FIRST_TIMESCALE = 1.
+SECOND_TIMESCALE = 15.
 FEH_LIM = (-1.4, 0.6)
 OFE_LIM = (-0.12, 0.48)
 
@@ -47,30 +48,36 @@ def main():
                                          smoothing=0.05)
     axs[2].plot(ofe_df / max(ofe_df), get_bin_centers(bin_edges), 'k-')
     
+    # Set up output directory
     output_dir = paths.data / "onezone" / "eta"
     if not output_dir.exists():
         output_dir.mkdir(parents=True)
     name = str(output_dir / "equilibrium")
     
+    # One-zone model parameters
     simtime = np.arange(0, 13.21, 0.01)
-    
-    eta_func = equilibrium_mass_loading(tau_star=2., tau_sfh=10., alpha_h_eq=0.2)
-    
     area = np.pi * ((RADIUS + ZONE_WIDTH)**2 - RADIUS**2)
+    ifr = twoinfall(
+        RADIUS, 
+        first_timescale=FIRST_TIMESCALE, 
+        second_timescale=SECOND_TIMESCALE, 
+        onset=ONSET
+    )
     
     sz = vice.singlezone(
         name = name,
-        func = twoinfall(
-            RADIUS, 
-            first_timescale=1., 
-            second_timescale=10., 
-            onset=ONSET),
+        func = ifr,
         mode = "ifr",
         **ONEZONE_DEFAULTS
     )
+    eta_func = equilibrium_mass_loading(
+        tau_star=2., 
+        tau_sfh=ifr.second.timescale, 
+        alpha_h_eq=0.2
+    )
     sz.eta = eta_func(RADIUS)
     print(eta_func(RADIUS))
-    sz.tau_star = twoinfall_sf_law(area, onset=ONSET)
+    sz.tau_star = twoinfall_sf_law(area, onset=ifr.onset)
     sz.run(simtime, overwrite=True)
     
     model_color = paultol.bright.colors[0]