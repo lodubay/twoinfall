077447cb20979b769f08e2a767b37e2bf9e51df9
Liam Dubay <liam.dubay@gmail.com>
2025-01-09 15:47:28 -0500

    simplify yield-outflow plot

--- src/scripts/yield_outflow.py
+++ src/scripts/yield_outflow.py
@@ -51,20 +51,14 @@ def main():
     params['RIa'] = dtds.plateau()
 
     # 3x yields, similar to J21
-    run_plot_model(axs, 3, params, eta_scale=1)
+    params['eta'] = 2.4
+    run_plot_model(axs, 3, params)
     # 2x yields
-    run_plot_model(axs, 2, params, eta_scale=1)
+    params['eta'] = 1.4
+    run_plot_model(axs, 2, params, yia_scale=1.1)
     # Solar yields, similar to W24
-    run_plot_model(axs, 1, params, eta_scale=1)
-    # Solar yields, x0.5 outflows
-    run_plot_model(axs, 1, params, eta_scale=0.5)
-    # Higher yIa
-    yia_scale = 1.3
-    run_plot_model(axs, 1, params, eta_scale=0.5, yia_scale=yia_scale, 
-                   c=paultol.vibrant.colors[3], ls='--', 
-                   label=r'$y_{\rm Ia}\times%s$' % yia_scale)
-    # Solar yields, no outflows
-    # run_plot_model(axs, 1, params, eta_scale=0.)
+    params['eta'] = 0.15
+    run_plot_model(axs, 1, params, yia_scale=1.4)
 
     # Plot APOGEE abundances + Leung et al. (2023) ages
     apogee_sample = APOGEESample.load()
@@ -88,7 +82,7 @@ def main():
     ax2.yaxis.set_major_locator(MultipleLocator(0.5))
     ax2.yaxis.set_minor_locator(MultipleLocator(0.1))
 
-    ax3.text(0.05, 0.95, r'$f^{\rm CC}_{\rm Fe}=%s$, Plateau DTD' % FE_CC_FRAC, 
+    ax3.text(0.05, 0.95, r'$y^{\rm CC}_{\rm Fe} / y^{\rm CC}_{\rm O}=%s$' % FE_CC_FRAC, 
              transform=ax3.transAxes, va='top')
     ax3.legend(frameon=False)
 
@@ -98,26 +92,27 @@ def main():
     ax4.yaxis.set_major_locator(MultipleLocator(0.2))
     ax4.yaxis.set_minor_locator(MultipleLocator(0.05))
 
+    ax5.text(0.05, 0.95, r'Plateau DTD', transform=ax5.transAxes, va='top')
     ax5.set_xlabel('Lookback Time [Gyr]')
-    ax5.legend(frameon=False)
+    # ax5.legend(frameon=False)
 
     fig.savefig(paths.figures / 'yield_outflow')
     plt.close()
 
 
-def run_plot_model(axs, scale, params, eta_scale=1., yia_scale=1., 
+def run_plot_model(axs, scale, params, yia_scale=1., 
                    label='', c=None, ls='-'):
     output_dir = paths.data / 'onezone' / 'yield_outflow'
     if not output_dir.exists():
         output_dir.mkdir(parents=True)
     
     scale_yields(scale, fe_cc_frac=FE_CC_FRAC, yia_scale=yia_scale)
-    params['eta'] = equilibrium_mass_loading(tau_sfh=SECOND_INFALL)(RADIUS)
-    params['eta'] *= eta_scale
+    # params['eta'] = equilibrium_mass_loading(tau_star=0.)(RADIUS)
+    # params['eta'] *= eta_scale
     RIa = vice.yields.sneia.settings['fe'] / SNIA_FE_YIELD
     if label == '':
-        label = r'$\eta=%s$, $y/Z_\odot = %s$' % (
-            round(params['eta'], 1), scale
+        label = r'$y/Z_\odot = %s$, $\eta=%s$' % (
+            scale, round(params['eta'], 2)
         )
 
     eta_func = lambda r: params['eta']
@@ -130,9 +125,9 @@ def run_plot_model(axs, scale, params, eta_scale=1., yia_scale=1.,
         dr=ZONE_WIDTH
     )
     # Run one-zone model
-    name = '%sxSol' % scale
-    if eta_scale != 1:
-        name += '_%sxEta' % eta_scale
+    name = '%sxSol_Eta%s' % (scale, params['eta'])
+    # if eta_scale != 1:
+    #     name += '_%sxEta' % eta_scale
     fullname = str(output_dir / name)
     sz = vice.singlezone(name=fullname,
                          func=ifr,
@@ -142,9 +137,15 @@ def run_plot_model(axs, scale, params, eta_scale=1., yia_scale=1.,
     sz.run(simtime, overwrite=True)
 
     # Plots
-    plot_abundance_history(axs[0], fullname, '[o/h]', label=label, c=c, ls=ls)
-    plot_abundance_history(axs[1], fullname, '[fe/h]', label=label, c=c, ls=ls)
-    plot_abundance_history(axs[2], fullname, '[o/fe]', label=label, c=c, ls=ls)
+    plot_abundance_history(
+        axs[0], fullname, '[o/h]', c=c, ls=ls,
+        label=r'$y^{\rm CC}_{\rm O} / Z_\odot = %s$, $\eta=%s$' % (
+            scale, params['eta']
+        ))
+    plot_abundance_history(
+        axs[1], fullname, '[fe/h]', c=c, ls=ls, 
+        label=r'$y^{\rm Ia}_{\rm Fe} = %s$' % round(vice.yields.sneia.settings['fe'], 4))
+    plot_abundance_history(axs[2], fullname, '[o/fe]', label='', c=c, ls=ls)
 
 
 def plot_abundance_history(axs, fullname, col, label='', c=None, ls='-'):
@@ -167,14 +168,14 @@ def scale_yields(scale, fe_cc_frac=0.35, yia_scale=1.):
     and
     $ y^{\rm Ia}_{\rm Fe} = S \times (1 - f_{\rm cc}) \times 
     Z_{\rm Fe,\odot} $,
-    where $f_{\rm cc}$ is the fraction of Solar Fe produced in CC SNe.
+    where $f_{\rm cc}$ is the ratio of Fe to O produced in CC SNe.
     
     Parameters
     ----------
     scale : float
         Scale of total yields relative to the Solar abundance.
     fe_cc_frac : float [default: 0.35]
-        Fraction of Solar Fe produced in CC SNe.
+        Ratio of Fe to O produced by CC SNe.
     
     """
     vice.yields.ccsne.settings['o'] = scale * vice.solar_z['o']
