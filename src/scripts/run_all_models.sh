b36a8cfb1942f9e7f221ee4cc4157d9b1412fea3
Liam Dubay <liam.dubay@gmail.com>
2025-01-31 13:45:01 -0500

    Update run_all_models.sh

--- src/scripts/run_all_models.sh
+++ src/scripts/run_all_models.sh
@@ -5,29 +5,29 @@ NSTARS=2
 NAME="yields/yZ1/diskmodel"
 echo $NAME
 python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ1
-echo "\n"
+echo ""
 
 NAME="migration_strength/strength50/diskmodel"
 echo $NAME
 python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ1 --migration-strength=5.0
-echo "\n"
+echo ""
 
 NAME="pre_enrichment/mh07_alpha00/diskmodel"
 echo $NAME
 python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ1 --pre-enrichment=-0.7
-echo "\n"
+echo ""
 
 NAME="yields/yZ2/diskmodel"
 echo $NAME
 python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ2
-echo "\n"
+echo ""
 
 NAME="dtd/powerlaw_yZ2/diskmodel"
 echo $NAME
 python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ2 --RIa=powerlaw
-echo "\n"
+echo ""
 
 NAME="pre_enrichment/mh07_alpha00_yZ2/diskmodel"
 echo $NAME
 python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ2 --pre-enrichment=-0.7
-echo "\n"
+echo ""
