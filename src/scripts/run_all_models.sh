2e76cb39e0f47ea98ec884f5bc0f8468739272c2
Liam Dubay <liam.dubay@gmail.com>
2025-02-07 11:24:15 -0500

    make default outflow prescription explicit

--- src/scripts/run_all_models.sh
+++ src/scripts/run_all_models.sh
@@ -4,30 +4,30 @@ NSTARS=2
 
 NAME="yields/yZ1/diskmodel"
 echo $NAME
-python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ1
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ1 --outflows=equilibrium
 echo ""
 
 NAME="migration_strength/strength50/diskmodel"
 echo $NAME
-python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ1 --migration-strength=5.0
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ1 --outflows=equilibrium --migration-strength=5.0
 echo ""
 
 NAME="pre_enrichment/mh07_alpha00/diskmodel"
 echo $NAME
-python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ1 --pre-enrichment=-0.7
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ1 --outflows=equilibrium --pre-enrichment=-0.7
 echo ""
 
 NAME="yields/yZ2/diskmodel"
 echo $NAME
-python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ2
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ2 --outflows=equilibrium
 echo ""
 
 NAME="dtd/powerlaw_yZ2/diskmodel"
 echo $NAME
-python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ2 --RIa=powerlaw
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ2 --outflows=equilibrium --RIa=powerlaw
 echo ""
 
 NAME="pre_enrichment/mh07_alpha00_yZ2/diskmodel"
 echo $NAME
-python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ2 --pre-enrichment=-0.7
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ2 --outflows=equilibrium --pre-enrichment=-0.7
 echo ""
