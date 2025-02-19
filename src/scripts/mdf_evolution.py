9a1728eb6faefdf0180403b0d3d1b7901f60ccca
Liam Dubay <liam.dubay@gmail.com>
2025-02-19 16:01:58 -0500

    explore more plot iterations

--- src/scripts/mdf_evolution.py
+++ src/scripts/mdf_evolution.py
@@ -16,11 +16,11 @@ from _globals import TWO_COLUMN_WIDTH
 import paths
 
 GALR_BINS = [3, 5, 7, 9, 11, 13]
-AGE_BINS = [0, 2, 4, 6, 8, 10]
+AGE_BINS = [1, 2, 4, 6, 8, 10]
 ABSZ_LIM = (0, 0.5)
 NBINS = 100
 SMOOTH_WIDTH = 0.1
-FEH_LIM = (-0.8, 0.6)
+XLIM = (-0.8, 0.6)
 YSCALE = 1e7
 AGE_COL = 'CN_AGE'
 
@@ -32,7 +32,14 @@ def main(style='paper', col='[fe/h]', cmap='coolwarm', smoothing=SMOOTH_WIDTH):
         sharex=True, sharey='row', 
         gridspec_kw={'hspace': 0.25, 'wspace': 0.1}
     )
+    # Get list of line colors
+    colors = get_color_list(plt.get_cmap(cmap), AGE_BINS)
+    # Add colorbar at left
+    cbar = setup_colorbar(fig, cmap=cmap, bounds=AGE_BINS,
+                          label=r'Stellar age [Gyr]',
+                          width=0.015, pad=0.015, labelpad=2)
 
+    # Plot multizone outputs
     output_names = [
         'yZ1/pre_enrichment/mh07_alpha00/diskmodel',
         'yZ2/pre_enrichment/mh07_alpha00/diskmodel'
@@ -41,23 +48,64 @@ def main(style='paper', col='[fe/h]', cmap='coolwarm', smoothing=SMOOTH_WIDTH):
         r'(a) $y/Z_\odot=1$, ${\rm %s}_{\rm CGM}=-0.7$' % capitalize_abundance(col),
         r'(b) $y/Z_\odot=2$, ${\rm %s}_{\rm CGM}=-0.7$' % capitalize_abundance(col)
     ]
-    # Plot multizone outputs
     apogee_data = APOGEESample.load()
     for i, output_name in enumerate(output_names):
         mzs = MultizoneStars.from_output(output_name)
         # mzs.model_uncertainty(apogee_data=apogee_data.data)
-        plot_mdf_evolution(mzs, axs[i], col=col, smoothing=smoothing, cmap=cmap,
-                           title=model_labels[i])
-    # Plot APOGEE data
-    plot_mdf_evolution(apogee_data, axs[-1], col=vice_to_apogee_col(col), 
-                       smoothing=smoothing, cmap=cmap, 
-                       title='(c) APOGEE ([C/N]-derived ages)', 
-                       age_col=AGE_COL, indicate_mode=True)
+        axes_title_background(axs[i,2], model_labels[i])
+        for j in range(len(GALR_BINS)-1):
+            galr_lim = GALR_BINS[j:j+2]
+            region_subset = mzs.region(galr_lim=galr_lim, absz_lim=ABSZ_LIM)
+            # for k in reversed(list(range(len(AGE_BINS)-1))):
+            for k in range(len(AGE_BINS)-1):
+                age_lim = AGE_BINS[k:k+2]
+                age_subset = region_subset.filter({'age': tuple(age_lim)})
+                # Plot normalized MDFs
+                mdf, bin_edges = age_subset.mdf(
+                    col, range=XLIM, bins=NBINS, smoothing=smoothing
+                )
+                axs[i,j].plot(
+                    get_bin_centers(bin_edges), mdf, 
+                    color=colors[k], linewidth=1, #alpha=alpha,
+                    label='%s - %s' % tuple(age_lim)
+                )
     
-    # Add colorbar at left
-    cbar = setup_colorbar(fig, cmap=cmap, bounds=AGE_BINS,
-                          label=r'Stellar age [Gyr]',
-                          width=0.015, pad=0.015, labelpad=2)
+    # Plot APOGEE data
+    if AGE_COL == 'L23_AGE':
+        axes_title_background(axs[-1,2], '(c) APOGEE (L23 ages)')
+    else:
+        axes_title_background(axs[-1,2], '(c) APOGEE ([C/N]-based ages)')
+    for j in range(len(GALR_BINS)-1):
+        galr_lim = GALR_BINS[j:j+2]
+        region_subset = apogee_data.region(galr_lim=galr_lim, absz_lim=ABSZ_LIM)
+        # for k in reversed(list(range(len(AGE_BINS)-1))):
+        if AGE_COL == 'CN_AGE':
+            # Indicate cut below [Fe/H] < -0.4 for URGB and RC stars
+            axs[-1,j].axvline(-0.4, color='gray', ls='--')
+            # Indicate hard cuts in [Fe/H] for all [C/N]-based ages
+            axs[-1,j].axvline(-0.9, color='gray', ls='-')
+            axs[-1,j].axvline(0.45, color='gray', ls='-')
+        youngest_mode = False
+        for k in range(len(AGE_BINS)-1):
+            age_lim = AGE_BINS[k:k+2]
+            age_subset = region_subset.filter({AGE_COL: tuple(age_lim)})
+            # Plot normalized MDFs
+            if age_subset.nstars > 100:# and age_lim[0] >= 1 and age_lim[1] <= 10:
+                mdf, bin_edges = age_subset.mdf(
+                    vice_to_apogee_col(col), 
+                    range=XLIM, bins=NBINS, smoothing=smoothing
+                )
+                axs[-1,j].plot(
+                    get_bin_centers(bin_edges), mdf, 
+                    color=colors[k], linewidth=1, #alpha=alpha,
+                    label='%s - %s' % tuple(age_lim)
+                )
+                # Indicate mode of youngest bin
+                if not youngest_mode:
+                    mode = np.argmax(mdf)
+                    axs[-1,j].axvline(bin_edges[mode:mode+2].mean(), 
+                                   color=colors[k], ls=':')
+                    youngest_mode = True
     
     for i in range(len(axs[0,:])):
         galr_lim = GALR_BINS[i:i+2]
@@ -84,54 +132,26 @@ def main(style='paper', col='[fe/h]', cmap='coolwarm', smoothing=SMOOTH_WIDTH):
     axs[1,0].set_ylabel('Normalized PDF')
     axs[0,0].xaxis.set_major_locator(MultipleLocator(0.5))
     axs[0,0].xaxis.set_minor_locator(MultipleLocator(0.1))
-    axs[0,0].set_xlim(FEH_LIM)
+    axs[0,0].set_xlim(XLIM)
     
     plt.savefig(paths.figures / 'mdf_evolution')
     plt.close()
 
 
-def plot_mdf_evolution(obj, axs, col='[fe/h]', smoothing=SMOOTH_WIDTH, 
-                       xlim=FEH_LIM, cmap='Spectral_r', nbins=NBINS, title='',
-                       age_col='age', indicate_mode=False):
-    """
-    Plot the MDF in bins of Rgal for different ages.
-    """
-    colors = get_color_list(plt.get_cmap(cmap), AGE_BINS)
-    # Row label with white background
-    axs[2].text(
-        0.5, 1.1, title, 
+def axes_title_background(ax, text, bgcolor='w', x=0.5, y=1.1):
+    ax.text(
+        x, y, text, 
         ha='center', va='top', 
-        transform=axs[2].transAxes,
+        transform=ax.transAxes,
         size=plt.rcParams['axes.titlesize'],
         bbox={
-            'facecolor': 'w',
+            'facecolor': bgcolor,
             'edgecolor': 'none',
             'boxstyle': 'round',
             'pad': 0.15,
             'alpha': 1.,
         }
     )
-    for i in range(len(GALR_BINS)-1):
-        galr_lim = GALR_BINS[i:i+2]
-        region_subset = obj.region(galr_lim=galr_lim, absz_lim=ABSZ_LIM)
-        # for j in reversed(list(range(len(AGE_BINS)-1))):
-        for j in range(len(AGE_BINS)-1):
-            age_lim = AGE_BINS[j:j+2]
-            age_subset = region_subset.filter({age_col: tuple(age_lim)})
-            # Plot normalized MDFs
-            if age_subset.nstars > 100:
-                mdf, bin_edges = age_subset.mdf(
-                    col, range=xlim, bins=nbins, smoothing=smoothing
-                )
-                axs[i].plot(
-                    get_bin_centers(bin_edges), mdf, 
-                    color=colors[j], linewidth=1, #alpha=alpha,
-                    label='%s - %s' % tuple(age_lim)
-                )
-                if indicate_mode and j == 0:
-                    mode = np.argmax(mdf)
-                    axs[i].axvline(bin_edges[mode:mode+2].mean(), 
-                                   color='gray', ls=':')
 
 
 if __name__ == '__main__':
