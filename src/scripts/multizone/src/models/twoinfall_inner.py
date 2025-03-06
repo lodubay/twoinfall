b9be9175ef89ab0047cb31bb3dba62893183cdbb
Liam Dubay <liam.dubay@gmail.com>
2025-03-06 15:09:12 -0500

    Update twoinfall_inner.py

--- src/scripts/multizone/src/models/twoinfall_inner.py
+++ src/scripts/multizone/src/models/twoinfall_inner.py
@@ -4,11 +4,13 @@ the two-infall model where the first infall is constrained to the inner
 galaxy (<9 kpc).
 """
 
-from .twoinfall import twoinfall
+from .twoinfall_expvar import twoinfall_expvar
+from ..._globals import THICK_DISK_SCALE_RADIUS, MAX_SF_RADIUS
+import math as m
 
-FIRST_MAX_RADIUS = 9 # kpc, maximum radius of the first infall
+FIRST_MAX_RADIUS = 3 # kpc, maximum radius of the first infall
 
-class twoinfall_inner(twoinfall):
+class twoinfall_inner(twoinfall_expvar):
     """
     Variant of the two-infall SFH where the first infall occurs only within the
     inner galaxy.
@@ -17,3 +19,8 @@ class twoinfall_inner(twoinfall):
         super().__init__(radius, **kwargs)
         if radius > FIRST_MAX_RADIUS:
             self.first.norm = 0
+        else:
+            # Increase inner normalization to maintain overall mass ratio
+            thick_disk_total = 1 - m.exp(- MAX_SF_RADIUS / THICK_DISK_SCALE_RADIUS)
+            thick_disk_inner = 1 - m.exp(- FIRST_MAX_RADIUS / THICK_DISK_SCALE_RADIUS)
+            self.first.norm *= thick_disk_total / thick_disk_inner
