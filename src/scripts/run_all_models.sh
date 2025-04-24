c4d93884d143d2216dc6edd337ab6ef3fb7e1f96
Liam Dubay <liam.dubay@gmail.com>
2025-04-24 16:03:47 -0400

    add best models

--- src/scripts/run_all_models.sh
+++ src/scripts/run_all_models.sh
@@ -23,6 +23,11 @@ echo $NAME
 python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ1 --evolution=$EVOLUTION --local-disk-ratio=0.5
 echo ""
 
+NAME="yZ1/best/cgm07_ratio025_eta04_migr36/diskmodel"
+echo $NAME
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ1 --evolution=$EVOLUTION --pre-enrichment=-0.7 --local-disk-ratio=0.25 --eta-solar=0.4 --migration-strength=3.6
+echo ""
+
 NAME="yZ2/fiducial/diskmodel"
 echo $NAME
 python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ2 --evolution=$EVOLUTION
@@ -41,4 +46,9 @@ echo ""
 NAME="yZ2/thick_thin_ratio/solar050/diskmodel"
 echo $NAME
 python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ2 --evolution=$EVOLUTION --local-disk-ratio=0.5
+echo ""
+
+NAME="yZ2/best/cgm07_ratio025_eta18_migr36/diskmodel"
+echo $NAME
+python -m multizone -f --nstars=$NSTARS --name=$NAME --yields=yZ2 --evolution=$EVOLUTION --pre-enrichment=-0.7 --local-disk-ratio=0.25 --eta-solar=1.8 --migration-strength=3.6
 echo ""
