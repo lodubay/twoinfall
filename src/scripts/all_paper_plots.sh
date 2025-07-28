a8e46f3e860707e09e38528946bc3e106b4536a9
Liam Dubay <liam.dubay@gmail.com>
2025-07-28 10:55:47 -0400

    update all plots script

--- src/scripts/all_paper_plots.sh
+++ src/scripts/all_paper_plots.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+# Generate APOGEE sample data table
+python apogee_sample.py
+
 # Tables and other output
-python apogee_regions_table.py
+python uncertainties.py
 python yields.py
 
 # Plots
-python yield_outflow.py
-python onezone_params.py
-python spitoni_comparison.py
-python sfe_prefactor.py
+python smooth_vs_twoinfall.py
 python star_formation_history.py
-python ofe_df_comparison.py
-python ofe_feh_density.py
-python abundance_evolution.py
+python infall_parameters.py
+python gas_abundance_evolution.py
+python stellar_abundance_evolution.py
 python mdf_evolution.py
-python onezone_sfe_hiatus.py
+python ofe_feh_density.py
+python ofe_distributions.py
+python ofe_feh_best.py
+python lmr_ages.py
+python sfe_hiatus.py
